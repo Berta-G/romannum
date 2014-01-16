e3af7cd155e8c6e27f30acf43aae84ec391de232
Berta <berta.gutierrez@gmail.com>
2014-01-16 12:21:10 +0000

    spec file added

--- romannum.rb
+++ romannum.rb
@@ -1,21 +1,26 @@
 def roman_to_decimal(romannum)
 
-	arr = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1}
+	nums = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1}
 
-	newarr = romannum.split("").map { |n| arr[n.to_sym] }
-	
+	newarr = romannum.split("").map { |n| nums[n.to_sym] }
+
+
+	# (0..newarr.length-2).inject(0) { |total, i| total +=  newarr[i] < newarr[i+1] ? -newarr[i] : newarr[i] } + newarr.last
+
 	i = 0 
-	total = 0
+
+
 
 	while i < newarr.length - 1
 		newarr[i] *= -1 if newarr[i] < newarr[i+1]  
 		i += 1
 	end
 
-	puts newarr.inject (:+)
+	newarr.inject (:+)
 	
 
 end
 
 
-roman_to_decimal(ARGV.first)
+# roman_to_decimal(ARGV.first)
+puts roman_to_decimal("I")
